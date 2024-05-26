321ebcd2f2a99d87732498ce43f1e541a317d548
David <khachatryandavid1@gmail.com>
2024-05-26 15:10:25 -0500

    update_api_backend_endpoint

--- client/src/api/endpoints.js
+++ client/src/api/endpoints.js
@@ -2,7 +2,7 @@
 // if more than one backend is used, they can be added here
 // however must be split into multiple objects for each server used 
 export const endpoints = {
-    main: process.env.NODE_ENV === "development" ? "http://localhost:8000/api" : "https://api.davidk.tech/api",
+    main: process.env.NODE_ENV === "development" ? "http://localhost:8000/api" : "https://api.davidk.tech:8443/api",
     nations: "/nations",
     continents: "/continents",
     report: "/report",
